d211ab69892c1ad8c6279d5f2926279dd15f1a77
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-03-02 12:51:26 +0300

    Add files via upload

---
+++ Kurucu-Metotlar/Kurucu-Metotlar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Kurucu_Metotlar
+{
+    class Canli
+    {
+        public string adı;
+        public string turu;
+        public int yası;
+        public int kilo;
+
+        public Canli()
+        {
+        }
+
+        public Canli(string Adı , string Turu , int Yası , int Kilo)
+        {
+            turu = Turu;
+            adı = Adı;
+            yası = Yası;
+            kilo = Kilo;
+        }
+
+        public void CanliYazdir()
+        {
+            Console.WriteLine("Canlının Adı :" +" "+ adı);
+            Console.WriteLine("Canlının Türü : " +" "+ turu);
+            Console.WriteLine("Canlının Yaşı :" +" "+ yası);
+            Console.WriteLine("Canlının Kilosu :" +" "+ kilo);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Canli canli = new Canli();
+            canli.adı = "Aslan";
+            canli.turu = "Panthera";
+            canli.yası = 10;
+            canli.kilo = 100;
+            canli.CanliYazdir();
+
+            Console.WriteLine("**********************************");
+
+            Canli canli2 = new Canli("Leylek" ,"Kuş" , 5 , 50 );
+            canli2.CanliYazdir();
+        }
+    }
+}
